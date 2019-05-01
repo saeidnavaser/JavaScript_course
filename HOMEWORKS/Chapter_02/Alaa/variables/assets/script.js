9c58f3706a867891b30438e002eab3fef14d8150
ahmedbhameed <ahmedbazy@gmail.com>
2019-05-01 11:06:45 +0200

    Reformating Alaa code

--- HOMEWORKS/Chapter_02/Alaa/variables/assets/script.js
+++ HOMEWORKS/Chapter_02/Alaa/variables/assets/script.js
@@ -1,6 +1,6 @@
 var ConePrice = 0.3;
 var IceCreamPrice = 1.5;
-var ArrayOfQuantity = [1,2,3];
+var ArrayOfQuantity = [1, 2, 3];
 var ChangeH3Text = ArrayOfQuantity[0] * IceCreamPrice + ConePrice + "€";
 var ChangeH4Text = ArrayOfQuantity[1] * IceCreamPrice + ConePrice + "€";
 var ChangeH5Text = ArrayOfQuantity[2] * IceCreamPrice + ConePrice + "€";
@@ -10,11 +10,9 @@ el[0].textContent = ChangeH3Text;
 el[1].textContent = ChangeH4Text;
 el[2].textContent = ChangeH5Text;
 
+console.log(el[0]);
+console.log(el[1]);
+console.log(el[2]);
 
-
-console.log(el[0])
-console.log(el[1])
-console.log(el[2])
-
-console.log(ArrayOfQuantity[0])
-console.log(ArrayOfQuantity[1])
+console.log(ArrayOfQuantity[0]);
+console.log(ArrayOfQuantity[1]);
