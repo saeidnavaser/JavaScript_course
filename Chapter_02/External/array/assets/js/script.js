16a170acb35e921966cba2f6215a91e55fac1254
saeid navaser <saeidnavaser@gmx.at>
2019-05-11 01:39:45 +0200

    new uodate

--- Chapter_02/External/array/assets/js/script.js
+++ Chapter_02/External/array/assets/js/script.js
@@ -1,9 +1,6 @@
-let arrayOfColors = ["green", "purple", "blue"]; // This is array creation
-let changeH1Text = "This is text in green color"; // This is shorthand variable with text
-console.log("Array of colors", arrayOfColors); // CHECKOUT WHAT IS GOING ON HERE BY PRINTING OUT RESULTS
+let arrayOfColors = ["green","purple","blue"]; 
+let changeH1Text = "This text is in green color";
 
-console.log("color in index 4: ", arrayOfColors[4]); // CHECKOUT WHAT IS GOING ON HERE BY PRINTING OUT RESULTS
-
-let el = document.getElementById("array"); // Grab html tag that has class name of "array"
-console.log("Element:", el); // CHECKOUT WHAT IS GOING ON HERE BY PRINTING OUT RESULTS
-el.textContent = changeH1Text; // Change text of the selected tag.
+let el = document.getElementsByClassName("array");
+console.log(el[0]);
+el[0].textContent = changeH1Text;
