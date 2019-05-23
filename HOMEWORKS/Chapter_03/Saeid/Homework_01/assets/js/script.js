050d9dc142c8a3ea72566015435d9347cf8078ad
saeid navaser <saeidnavaser@gmx.at>
2019-05-23 06:25:41 +0200

    change in Chapter 03 and add Chapter 04

--- HOMEWORKS/Chapter_03/Saeid/Homework_01/assets/js/script.js
+++ HOMEWORKS/Chapter_03/Saeid/Homework_01/assets/js/script.js
@@ -1,57 +1,62 @@
-// Start to write a function for "Basketball surface" calculating
-const surfaceOfBalls = {
-    radius: [5, 3.35, 4.3],
-    get basketBallArea() {
-        let basket = 4 * Math.PI * this.radius[0] * this.radius[0]; // If we use Math then result is 314.1592653589793 and that as a result not a basketball´s area!(between 313.5 & 314) but I use it as a correct to print a msg!
-        let msg_01 = ""; // Message that we need to print it
+//Create h1 tag for title of page and write the title
+let h1OfTitle = document.createElement("h1"); // h1 tag
+textOfTitle = document.createTextNode("Surface Of Balls"); //text of title
+h1OfTitle.appendChild(textOfTitle);
+document.body.appendChild(h1OfTitle);
 
-        // Here is the function of basketball
-        function basketBall() {
-            msg_01 += "Radius of 5 cm2 = ";
-        }
+let radius = ["5 cm2", "3.35 cm2", "4.3 cm2"];
+let balls = ["Basketball", "Tennis ball", "Football"];
+//let msgOfRadius = "Radius: "; //(This is correct too but I try different way to print the Msg )
+//let msgOfBall = "This is a ";
 
-        if (basket >= 314) {
-            basketBall();
-            msg_01 += "This is a basketball.";
-        }
-        let el = document.getElementById("basketball");
-        el.innerHTML = msg_01;
-    },
-    get tennisBallArea() {
-        let tennis = 4 * Math.PI * this.radius[1] * this.radius[1];
-        let msg_02 = ""; // Message that we need to print it
+// Write msg of Basketball
+let msgOfBasket = "<h3>Radius: </h3><p>" + radius[0] + "</p>";
+msgOfBasket += "<h3>This is a </h3><p>" + balls[0] + ".</p>";
 
-        // Here is the function of tennis ball
-        function tennisBall() {
-            msg_02 += "Radius of 3.35 cm2 = ";
-        }
+// Write msg of Tennis ball
+let msgOfTennis = "<h3>Radius: </h3><p>" + radius[1] + "</p>";
+msgOfTennis += "<h3>This is a </h3><p>" + balls[1] + ".</p>";
 
-        if (tennis >= 141) {
-            tennisBall();
-            msg_02 += "This is a tennis ball.";
-        }
-        let el = document.getElementById("tennisball");
-        el.innerHTML = msg_02;
-    },
-    get footBallArea() {
-        let foot = 4 * Math.PI * this.radius[2] * this.radius[2];
-        let msg_03 = ""; // Message that we need to print it
+// Write msg of Football
+let msgOfFoot = "<h3>Radius: </h3><p>" + radius[2] + "</p>";
+msgOfFoot += "<h3>This is a </h3><p>" + balls[2] + ".</p>";
 
-        // Here is the function of football
-        function footBall() {
-            msg_03 += "Radius of 4.3 cm2 = ";
-        }
+//Create function for cards
+const createCard = () => {
+  // Create container a then 3 cards for our balls
+  let cardOfContainer = document.createElement("div"); // div of container
+  cardOfContainer.setAttribute("class", "container"); // class of container
 
-        if (foot >= 232) {
-            footBall();
-            msg_03 += "This is a football.";
-        }
-        let el = document.getElementById("football");
-        el.innerHTML = msg_03;
-    },
+  // first card
+  let cardOfBasket = document.createElement("div"); // Create a div for Card
+  cardOfBasket.setAttribute("class", "card"); // Create attribute of class
+  cardOfBasket.innerHTML +=
+    "<img src='./assets/img/basketball.jpg' alt='basketball img'  class='img'>";
+  /*cardOfBasket.innerHTML +=
+    msgOfRadius + radius[0] + "<br></br>" + msgOfBall + balls[0] + ".";*/
+  cardOfBasket.innerHTML += msgOfBasket; // Print the Msg
+
+  //second card
+  let cardOfTennis = document.createElement("div"); // Create a div for Card
+  cardOfTennis.setAttribute("class", "card"); // Create attribute of class
+  cardOfTennis.innerHTML +=
+    "<img src='./assets/img/tennisball.jpg' alt='basketball img'  class='img'>";
+  /*cardOfTennis.innerHTML +=
+    msgOfRadius + radius[1] + "<br></br>" + msgOfBall + balls[1] + ".";*/
+  cardOfTennis.innerHTML += msgOfTennis; // Print the Msg
+
+  //third card
+  let cardOfFoot = document.createElement("div"); // Create a div for Card
+  cardOfFoot.setAttribute("class", "card"); // Create attribute of class
+  cardOfFoot.innerHTML +=
+    "<img src='./assets/img/football.jpg' alt='basketball img'  class='img'>";
+  /*cardOfFoot.innerHTML +=
+    msgOfRadius + radius[2] + "<br></br>" + msgOfBall + balls[2] + ".";*/
+  cardOfFoot.innerHTML += msgOfFoot; // Print the Msg
+
+  cardOfContainer.appendChild(cardOfBasket);
+  cardOfContainer.appendChild(cardOfTennis);
+  cardOfContainer.appendChild(cardOfFoot);
+  document.body.appendChild(cardOfContainer);
 };
-
-
-console.log(surfaceOfBalls.basketBallArea);
-console.log(surfaceOfBalls.tennisBallArea);
-console.log(surfaceOfBalls.footBallArea);
+createCard();
---
+++ HOMEWORKS/Chapter_04/Saeid/Homework/assets/js/script.js
@@ -0,0 +1,163 @@
+//Create h1 tag for title of page and write the title
+let h1OfTitle = document.createElement("h1"); // h1 tag
+textOfTitle = document.createTextNode("Cats of Chapter 04"); //text of title
+h1OfTitle.appendChild(textOfTitle);
+document.body.appendChild(h1OfTitle);
+
+// Array of cats
+let cats = [
+  {
+    breeds: [],
+    id: "2f9",
+    url: "https://26.media.tumblr.com/tumblr_m3o5vhPKBa1rtuomto1_250.jpg",
+    width: 500,
+    height: 333
+  },
+  {
+    breeds: [],
+    id: "3h0",
+    url: "https://25.media.tumblr.com/tumblr_m3kg02GkLw1qjc1a7o1_250.jpg",
+    width: 900,
+    height: 600
+  },
+  {
+    breeds: [],
+    id: "3v7",
+    url: "https://cdn2.thecatapi.com/images/3v7.gif",
+    width: 245,
+    height: 245
+  },
+  {
+    breeds: [],
+    id: "58l",
+    url: "https://25.media.tumblr.com/tumblr_kop8wzgxHa1qzv5pwo1_250.jpg",
+    width: 385,
+    height: 500
+  },
+  {
+    breeds: [],
+    id: "9qn",
+    url: "https://24.media.tumblr.com/tumblr_m27vjmHY8Y1qh66wqo1_250.png",
+    width: 600,
+    height: 800
+  },
+  {
+    breeds: [],
+    id: "aad",
+    url: "https://24.media.tumblr.com/tumblr_m1vu8mrE0z1qze0hyo1_250.jpg",
+    width: 1344,
+    height: 1008
+  },
+  {
+    breeds: [],
+    id: "ae4",
+    url: "https://cdn2.thecatapi.com/images/ae4.gif",
+    width: 430,
+    height: 242
+  },
+  {
+    breeds: [],
+    id: "auf",
+    url: "https://25.media.tumblr.com/tumblr_m0v0feUY3X1qzwfgto1_250.png",
+    width: 561,
+    height: 554
+  },
+  {
+    breeds: [],
+    id: "dqn",
+    url: "https://25.media.tumblr.com/tumblr_m4f8t9JGy11qejbiro1_250.jpg",
+    width: 560,
+    height: 480
+  }
+];
+
+// Create function for our cards
+const divOfContainerAndCard = () => {
+  // Create container
+  let cardOfContainer = document.createElement("div"); // div of container
+  cardOfContainer.setAttribute("class", "container"); // class of container
+
+  // first card
+  let cardOfFirstCat = document.createElement("div"); // Create a div for Card
+  cardOfFirstCat.setAttribute("class", "card"); // Create attribute of class
+  cardOfFirstCat.innerHTML +=
+    "<img src='https://26.media.tumblr.com/tumblr_m3o5vhPKBa1rtuomto1_250.jpg'  class='img'>";
+  cardOfFirstCat.innerHTML +=
+    "<h3>name: </h3><p>" + cats[0].url.slice(28, 63) + "</p>"; //Print the Msg
+
+  // second card
+  let cardOfSecondCat = document.createElement("div"); // Create a div for Card
+  cardOfSecondCat.setAttribute("class", "card"); // Create attribute of class
+  cardOfSecondCat.innerHTML +=
+    "<img src='https://25.media.tumblr.com/tumblr_m3kg02GkLw1qjc1a7o1_250.jpg'  class='img'>";
+  cardOfSecondCat.innerHTML +=
+    "<h3>name: </h3><p>" + cats[1].url.slice(28, 63) + "</p>"; //Print the Msg
+
+  // third card
+  let cardOfThirdCat = document.createElement("div"); // Create a div for Card
+  cardOfThirdCat.setAttribute("class", "card"); // Create attribute of class
+  cardOfThirdCat.innerHTML +=
+    "<img src='https://cdn2.thecatapi.com/images/3v7.gif'  class='img'>";
+  cardOfThirdCat.innerHTML +=
+    "<h3>name: </h3><p>" + cats[2].url.slice(34, 43) + "</p>"; //Print the Msg
+
+  // fourth card
+  let cardOfFourthCat = document.createElement("div"); // Create a div for Card
+  cardOfFourthCat.setAttribute("class", "card"); // Create attribute of class
+  cardOfFourthCat.innerHTML +=
+    "<img src='https://25.media.tumblr.com/tumblr_kop8wzgxHa1qzv5pwo1_250.jpg'  class='img'>";
+  cardOfFourthCat.innerHTML +=
+    "<h3>name: </h3><p>" + cats[3].url.slice(28, 63) + "</p>"; //Print the Msg
+
+  // fifth card
+  let cardOfFifthCat = document.createElement("div"); // Create a div for Card
+  cardOfFifthCat.setAttribute("class", "card"); // Create attribute of class
+  cardOfFifthCat.innerHTML +=
+    "<img src='https://24.media.tumblr.com/tumblr_m27vjmHY8Y1qh66wqo1_250.png'  class='img'>";
+  cardOfFifthCat.innerHTML +=
+    "<h3>name: </h3><p>" + cats[4].url.slice(28, 63) + "</p>"; //Print the Msg
+
+  // sixth card
+  let cardOfSixthCat = document.createElement("div"); // Create a div for Card
+  cardOfSixthCat.setAttribute("class", "card"); // Create attribute of class
+  cardOfSixthCat.innerHTML +=
+    "<img src='https://24.media.tumblr.com/tumblr_m1vu8mrE0z1qze0hyo1_250.jpg' class='img'>";
+  cardOfSixthCat.innerHTML +=
+    "<h3>name: </h3><p>" + cats[5].url.slice(28, 63) + "</p>"; //Print the Msg
+
+  // seventh card
+  let cardOfSeventhCat = document.createElement("div"); // Create a div for Card
+  cardOfSeventhCat.setAttribute("class", "card"); // Create attribute of class
+  cardOfSeventhCat.innerHTML +=
+    "<img src='https://cdn2.thecatapi.com/images/ae4.gif'  class='img'>";
+  cardOfSeventhCat.innerHTML +=
+    "<h3>name: </h3><p>" + cats[6].url.slice(34, 43) + "</p>"; //Print the Msg
+
+  // eighth card
+  let cardOfEighthCat = document.createElement("div"); // Create a div for Card
+  cardOfEighthCat.setAttribute("class", "card"); // Create attribute of class
+  cardOfEighthCat.innerHTML +=
+    "<img src='https://25.media.tumblr.com/tumblr_m0v0feUY3X1qzwfgto1_250.png' class='img'>";
+  cardOfEighthCat.innerHTML +=
+    "<h3>name: </h3><p>" + cats[7].url.slice(28, 63) + "</p>"; //Print the Msg
+
+  // ninth card
+  let cardOfNinthCat = document.createElement("div"); // Create a div for Card
+  cardOfNinthCat.setAttribute("class", "card"); // Create attribute of class
+  cardOfNinthCat.innerHTML +=
+    "<img src='https://25.media.tumblr.com/tumblr_m4f8t9JGy11qejbiro1_250.jpg'  class='img'>";
+  cardOfNinthCat.innerHTML +=
+    "<h3>name: </h3><p>" + cats[8].url.slice(28, 63) + "</p>"; //Print the Msg
+
+  cardOfContainer.appendChild(cardOfFirstCat);
+  cardOfContainer.appendChild(cardOfSecondCat);
+  cardOfContainer.appendChild(cardOfThirdCat);
+  cardOfContainer.appendChild(cardOfFourthCat);
+  cardOfContainer.appendChild(cardOfFifthCat);
+  cardOfContainer.appendChild(cardOfSixthCat);
+  cardOfContainer.appendChild(cardOfSeventhCat);
+  cardOfContainer.appendChild(cardOfEighthCat);
+  cardOfContainer.appendChild(cardOfNinthCat);
+  document.body.appendChild(cardOfContainer);
+};
+divOfContainerAndCard();
