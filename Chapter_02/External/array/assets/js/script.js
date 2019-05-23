54452e678b081391773b7f36a5e6cc2f59df61dd
saeid navaser <saeidnavaser@gmx.at>
2019-05-23 06:15:25 +0200

    New change in Homework of Chapter 03 and create homework of Chapter 04

--- Chapter_02/External/array/assets/js/script.js
+++
@@ -1,6 +0,0 @@
-let arrayOfColors = ["green","purple","blue"]; 
-let changeH1Text = "This text is in green color";
-
-let el = document.getElementsByClassName("array");
-console.log(el[0]);
-el[0].textContent = changeH1Text;
